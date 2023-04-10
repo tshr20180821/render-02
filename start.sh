88fe7bbcded7f424fc3d7cb1b721edf45cda5890
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-11 06:21:42 +0900

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
+sed -i s/__DEPLOY_DATETIME__/${DEPLOY_DATETIME}/ /etc/apache2/sites-enabled/apache.conf
+
+cat /etc/apache2/sites-enabled/apache.conf
+
+echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
+
+. /etc/apache2/envvars
+exec /usr/sbin/apache2 -DFOREGROUND
