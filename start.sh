a6ced65ff8342cd61328fb8659280eb57090b370
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-11 16:08:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-cat /var/www/html/auth/test20230411.php
+# cat /var/www/html/auth/test20230411.php
 
 export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 sed -i s/__DEPLOY_DATETIME__/${DEPLOY_DATETIME}/ /etc/apache2/sites-enabled/apache.conf
