6848fc4bc8c96400192ab3708f82c88210df73f2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-16 09:41:52 +0900

    Update check_magazine.php

--- auth/check_magazine.php
+++ auth/check_magazine.php
@@ -136,7 +136,7 @@ function access_library($pdo_sqlite_, $symbol_, $title_, $bibid_last_)
         if ($rc === 0) {
             continue;
         }
-        $log->info(print_r($match, true));
+        // $log->info(print_r($match, true));
         $idx = $match[1];
     }
     
