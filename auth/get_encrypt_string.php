74f1b48cc77b70ce73c5be51f772d8048e0031b2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-18 10:08:07 +0900

    Create get_encrypt_string.php

---
+++ auth/get_encrypt_string.php
@@ -0,0 +1,27 @@
+<?php
+
+include('/usr/src/app/MyUtils.php');
+
+$log = new Log();
+$mu = new MyUtils();
+
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+$log->info("START ${requesturi}");
+
+$html = <<< __HEREDOC__
+<html><body>
+<form method="POST" action="./get_encrypt_string.php">
+<input type="text" name="original" />
+<input type="submit" /> 
+</form>
+</body></html>
+__HEREDOC__;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $log->warn($mu->get_encrypt_string($_POST['original']));
+} else {
+    echo $html;
+}
+
+$log->info('FINISH ' . substr((microtime(true) - $time_start), 0, 7) . 's');
