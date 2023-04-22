60878323a7eb192530fd8c0dc0462ac445ffecdb
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-23 00:41:43 +0900

    Update check_magazine.php

--- auth/check_magazine.php
+++ auth/check_magazine.php
@@ -66,7 +66,9 @@ SELECT COUNT('X')
  WHERE M1.reserve = 1
 __HEREDOC__;
     
-    if ($pdo_sqlite->query($sql_select)->fetchColumn() != 0) {
+    $rc = $pdo_sqlite->query($sql_select)->fetchColumn();
+    $log->debug("rc : ${rc}");
+    if ($rc != 0) {
         $cmd = 'curl -u ' . $_ENV['BASIC_USER'] . ':' . $_ENV['BASIC_PASSWORD'] . ' https://' . $_ENV['RENDER_EXTERNAL_HOSTNAME'] . '/auth/reserve_magazine.php';
         $mu->cmd_execute($cmd);
     }
