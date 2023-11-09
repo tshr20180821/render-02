25aff10478fb8c1e7d752607169153781c82dd38
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-09 16:11:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-# apt-get update
-# apt-get -y upgrade
-
 dpkg -l
+
+echo ${LDFLAGS}
 
 # phpMyAdmin
 export BLOWFISH_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -15,8 +14,6 @@ export USER_AGENT=$(curl -sS https://raw.githubusercontent.com/tshr20180821/file
 export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 sed -i s/__RENDER_EXTERNAL_HOSTNAME__/${RENDER_EXTERNAL_HOSTNAME}/ /etc/apache2/sites-enabled/apache.conf
 sed -i s/__DEPLOY_DATETIME__/${DEPLOY_DATETIME}/ /etc/apache2/sites-enabled/apache.conf
-
-# cat /etc/apache2/sites-enabled/apache.conf
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
