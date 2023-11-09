9863806003ee2de382db8170f74e4b1e8f74090c
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-09 09:58:47 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ apt-get -y upgrade
 
 dpkg -l
 
+ls -lang
+
 # phpMyAdmin
 export BLOWFISH_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
@@ -16,7 +18,7 @@ export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 sed -i s/__RENDER_EXTERNAL_HOSTNAME__/${RENDER_EXTERNAL_HOSTNAME}/ /etc/apache2/sites-enabled/apache.conf
 sed -i s/__DEPLOY_DATETIME__/${DEPLOY_DATETIME}/ /etc/apache2/sites-enabled/apache.conf
 
-cat /etc/apache2/sites-enabled/apache.conf
+# cat /etc/apache2/sites-enabled/apache.conf
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
 
@@ -28,7 +30,6 @@ php --version | head -n 1 >>VERSION.txt
 
 VERSION=$(cat VERSION.txt)
 rm VERSION.txt
-rm -f /usr/src/app/*.java
 
 curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
   -d "text=${VERSION}" -d "channel=${SLACK_CHANNEL_01}" https://slack.com/api/chat.postMessage >/dev/null \
